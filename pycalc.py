02731cdb5e1c1a4f64bf9a47cb5b6c1231bea7d0
Sondre <31799749+sonlar@users.noreply.github.com>
2023-11-09 21:02:36 +0100

    Add basic calc layout

--- pycalc.py
+++ pycalc.py
@@ -1,4 +1,53 @@
-from tkinter import Button, Label, Tk
+from tkinter import GROOVE, Button, Frame, Label, Tk
 
 window = Tk()
+
+display = Label(window, text="9+9", width=25, relief=GROOVE)
+display.grid(row=0, column=0)
+
+rad1 = Frame(window)
+rad1.grid(row=1, column=0)
+rad2 = Frame(window)
+rad2.grid(row=2, column=0)
+rad3 = Frame(window)
+rad3.grid(row=3, column=0)
+rad4 = Frame(window)
+rad4.grid(row=4, column=0)
+
+button7 = Button(rad1, text=7, width=5)
+button7.grid(row=1, column=0)
+button8 = Button(rad1, text=8, width=5)
+button8.grid(row=1, column=1)
+button9 = Button(rad1, text=9, width=5)
+button9.grid(row=1, column=2)
+buttonaddition = Button(rad1, text="+", width=5)
+buttonaddition.grid(row=1, column=3)
+
+button4 = Button(rad2, text=4, width=5)
+button4.grid(row=2, column=0)
+button5 = Button(rad2, text=5, width=5)
+button5.grid(row=2, column=1)
+button6 = Button(rad2, text=6, width=5)
+button6.grid(row=2, column=2)
+buttonsubtraction = Button(rad2, text="-", width=5)
+buttonsubtraction.grid(row=2, column=3)
+
+button1 = Button(rad3, text=1, width=5)
+button1.grid(row=3, column=0)
+button2 = Button(rad3, text=2, width=5)
+button2.grid(row=3, column=1)
+button3 = Button(rad3, text=3, width=5)
+button3.grid(row=3, column=2)
+buttonmultiplication = Button(rad3, text="*", width=5)
+buttonmultiplication.grid(row=3, column=3)
+
+button0 = Button(rad4, text=0, width=5)
+button0.grid(row=4, column=0)
+buttonc = Button(rad4, text="C", width=5)
+buttonc.grid(row=4, column=1)
+buttonequal = Button(rad4, text="=", width=5)
+buttonequal.grid(row=4, column=2)
+buttondivision = Button(rad4, text="/", width=5)
+buttondivision.grid(row=4, column=3)
+
 window.mainloop()
